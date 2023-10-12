08c3e5fc95aec9f316b5973f2d7416e5a3914abe
dhia85 <dhiaeddine.bensaada@esprit.tn>
2023-10-12 11:21:42 +0100

    firstpush

---
+++ src/main/java/com/example/simulateurmarche/SimulateurMarcheApplication.java
@@ -0,0 +1,13 @@
+package com.example.simulateurmarche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulateurMarcheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimulateurMarcheApplication.class, args);
+    }
+
+}
